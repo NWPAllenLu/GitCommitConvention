7705ed3e6b970b8b38d426e8b5dd4badc64e9749
Allen.lu <allen.lu@neweb.com.tw>
2022-07-11 11:40:50 +0800

    feat(Git範例): 新增兩個輸入參數與output顯示方式
    新增排序方式以及查詢來源方式兩個參數,並調整輸出格式
    
    Issue: 12337

--- testFun.php
+++ testFun.php
@@ -23,18 +23,81 @@ const API_VERSION = 'v3.3/';
 /**
  * pchome 24h 其他參數
  */
-const API_PARAGRAPH = 'all/results';
+const API_PARAGRAPH = 'results';
+
+/**
+ * 排序方式
+ * bySale = 有貨優先, 
+ * byRanking = 精準度, 
+ * byPriceDc = 價格由高到底, 
+ * byPriceAc = 價格由低到高
+ * newProd = 新商品
+ */
+const SORT_TYPE = [
+    'bySale' => 'sale/dc',
+    'byRanking' => 'rnk/dc',
+    'byPriceDC' => 'prc/dc',
+    'byPriceAC' => 'prc/ac',
+    'newProd' => 'new/dc'
+];
+
+/**
+ * 查詢來源
+ * all = 全部, 
+ * by24h = 查24h商店, 
+ * by24hBooks = 查24h書店, 
+ * byVendor = 廠商出貨
+ * byTour = pchome旅遊
+ * mercari = 日本mercari //TODO 這是另一個API要改寫
+ */
+const SOURCE_TYPE = [
+    'all' => 'all/',
+    'by24h' => '24h/',
+    'by24hBooks' => '24b/',
+    'byVendor' => 'vdr/',
+    'byTour' => 'tour/',
+    // 'mercari' => 'mercari/'
+];
+
+$result = findPchomeEC($argv);
+
+echo '查詢[' . $argv[1] . ']的結果共: ' . $result['count'] . '筆' . PHP_EOL;
+
+foreach ($result['prod'] as $row) {
+
+    if (count($row['couponActid']) > 0) {
+        $couponId = PHP_EOL . '　　　這個有優惠券:' . json_encode($row['couponActid']);
+    } else {
+        $couponId = '';
+    }
+
+    echo "https://24h.pchome.com.tw/prod/{$row['Id']}   商品名稱:{$row['name']},價格:{$row['price']}(原價{$row['originPrice']}){$couponId}" . PHP_EOL;
+}
 
 
-$apiUrl = API_DOMAIN . API_FUNCTION . API_VERSION . API_PARAGRAPH;
+function findPchomeEC($argv = [])
+{
+    $key = $argv[1];
+    $type = $argv[2];
+    $sort = $argv[3];
 
-$apiParam = '?q=' . $argv[1];
+    $searchKey = '?q=' . $key . '&';
+    $sourceType = SOURCE_TYPE[$type];
+    $sortType = SORT_TYPE[$sort];
 
-$apiUrl .= $apiParam;
+    $apiUrl = API_DOMAIN . API_FUNCTION . API_VERSION . $sourceType . API_PARAGRAPH . $searchKey . $sortType;
 
-$result = grabIt($apiUrl, 'POST');
+    $result = grabIt($apiUrl, 'GET');
 
-echo $result;
+    $result = json_decode($result, true);
+
+    $formatResult = [
+        'prod' => $result['prods'],
+        'count' => count($result['prods'])
+    ];
+
+    return $formatResult;
+}
 
 /**
  * cURL function
