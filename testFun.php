e612af07badc18041e1c93d8e96534bf3b14c49a
Allen.lu <allen.lu@neweb.com.tw>
2022-07-11 10:31:45 +0800

    feat(Git範例): 範例程式初始化
    Git Commit Message基本初始化
    
    issue: 12235

---
+++ testFun.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * 20220711 ALlen init
+ * 以pchome 24h 爬蟲為例子
+ */
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=帳篷&page=1&sort=rnk/dc',
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_ENCODING => '',
+    CURLOPT_MAXREDIRS => 10,
+    CURLOPT_TIMEOUT => 0,
+    CURLOPT_FOLLOWLOCATION => true,
+    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+    CURLOPT_CUSTOMREQUEST => 'GET',
+    CURLOPT_HTTPHEADER => array(
+        'Cookie: U=c7c9be50ba855fb7c2566f0266a1d7d5ba792931'
+    ),
+));
+
+$response = curl_exec($curl);
+
+curl_close($curl);
+echo $response;
