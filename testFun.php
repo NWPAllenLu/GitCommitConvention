2862e9f0f366487e4a5442bae30b176b96d72675
Allen.lu <allen.lu@neweb.com.tw>
2022-07-11 10:57:41 +0800

    refactor(Git範例): 爬蟲程式重構
    將Curl功能抽出成獨立Function,並且把API URL整理成const
    
    issue: 12235

--- testFun.php
+++ testFun.php
@@ -5,25 +5,60 @@
  * 以pchome 24h 爬蟲為例子
  */
 
-$param = $argv[1];
+/**
+ * pchome 24h url
+ */
+const API_DOMAIN = 'https://ecshweb.pchome.com.tw/';
 
-$curl = curl_init();
+/**
+ * pchome 24h func
+ */
+const API_FUNCTION = 'search/';
 
-curl_setopt_array($curl, array(
-    CURLOPT_URL => 'https://ecshweb.pchome.com.tw/search/v3.3/all/results?q=' . $param . '&page=1&sort=rnk/dc',
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_ENCODING => '',
-    CURLOPT_MAXREDIRS => 10,
-    CURLOPT_TIMEOUT => 0,
-    CURLOPT_FOLLOWLOCATION => true,
-    CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-    CURLOPT_CUSTOMREQUEST => 'GET',
-    CURLOPT_HTTPHEADER => array(
-        'Cookie: U=c7c9be50ba855fb7c2566f0266a1d7d5ba792931'
-    ),
-));
+/**
+ * pchome 24h 版本號
+ */
+const API_VERSION = 'v3.3/';
 
-$response = curl_exec($curl);
+/**
+ * pchome 24h 其他參數
+ */
+const API_PARAGRAPH = 'all/results';
 
-curl_close($curl);
-echo $response;
+
+$apiUrl = API_DOMAIN . API_FUNCTION . API_VERSION . API_PARAGRAPH;
+
+$apiParam = '?q=' . $argv[1];
+
+$apiUrl .= $apiParam;
+
+$result = grabIt($apiUrl);
+
+echo $result;
+
+/**
+ * cURL function
+ */
+function grabIt($apiUrl)
+{
+    $curl = curl_init();
+
+    curl_setopt_array(
+        $curl,
+        [
+            CURLOPT_URL => $apiUrl,
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => 'GET',
+        ]
+    );
+
+    $response = curl_exec($curl);
+    curl_close($curl);
+
+    return $response;
+}
