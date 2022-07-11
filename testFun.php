72003a3b2bd11d7f7929229274d0f2c96e71dbd9
Allen.lu <allen.lu@neweb.com.tw>
2022-07-11 11:06:05 +0800

    fix(Git範例): 修正CURL HTTP VERB的行為
    此API只接受GET,前一版誤植為POST,須調整回GET
    
    issue: 12237

--- testFun.php
+++ testFun.php
@@ -32,7 +32,7 @@ $apiParam = '?q=' . $argv[1];
 
 $apiUrl .= $apiParam;
 
-$result = grabIt($apiUrl, 'POST');
+$result = grabIt($apiUrl, 'GETR');
 
 echo $result;
 
@@ -54,6 +54,9 @@ function grabIt($apiUrl, $request)
             CURLOPT_FOLLOWLOCATION => true,
             CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
             CURLOPT_CUSTOMREQUEST => $request,
+            CURLOPT_HTTPHEADER => [
+                'Cookie: U=c7c9be50ba855fb7c2566f0266a1d7d5ba792931'
+            ],
         ]
     );
 
